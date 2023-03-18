654d81893313db3e30e3625bd00d23e28a138b13
Zunair Sarwar <92993165+Zunairzz@users.noreply.github.com>
2023-03-18 12:53:00 +0500

    feat: login page created

--- src/component/CustomFooter.jsx
+++ src/component/CustomFooter.jsx
@@ -1,7 +1,7 @@
 export const CustomFooter = () => {
     return (
         <div>
-            <h1>This is Footer</h1>
+            <h1 style={{color: "white", textAlign: "center"}}>This is Footer</h1>
         </div>
     )
 }
