1a108f199bf3ed7b76d8cd1ba0a23f017b8b6bb0
upgenics <zunair.sarwar1@gmail.com>
2023-05-11 18:01:11 +0500

    feat: append

---
+++ src/component/imagesData.js
@@ -0,0 +1,18 @@
+export const imageData  = [
+    {
+        id: 1,
+        imageUrl: 'https://res.cloudinary.com/dyo1h8cbk/image/upload/v1683800293/sdcxrkoyhaarvdtzclf0.jpg'
+    },
+    {
+        id: 2,
+        imageUrl: 'https://res.cloudinary.com/dyo1h8cbk/image/upload/v1683800293/sdcxrkoyhaarvdtzclf0.jpg'
+    },
+    {
+        id: 3,
+        imageUrl: ''
+    },
+    {
+        id: 4,
+        imageUrl: ''
+    },
+]
--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -1,24 +1,61 @@
 import {Base} from "../component/Base";
-import {Card, Col, Container, Row} from "reactstrap";
+import {Button, Card, Col, Container, Row, Table} from "reactstrap";
+import {imageData} from '../component/imagesData';
+import {useState} from "react";
+import {Image} from "react-bootstrap";
 
 export function Images() {
-    function copyToClipboard(element) {
-        var $temp = $("<input>");
-        $("body").append($temp);
-        $temp.val($(element).text()).select();
-        document.execCommand("copy");
-        $temp.remove();
-    }
+
+    const [copySuccess, setCopySuccess] = useState('');
+
+    const handleCopyClick = (imageUrl) => {
+        // const textToCopy = "This is the text to copy.";
+        const text = imageUrl;
+        console.log(imageUrl)
+        navigator.clipboard.writeText(imageUrl);
+        setCopySuccess('Copied to clipboard!');
+    };
 
     return (
         <div>
             <Base>
                 <h1>All Images</h1>
                 <Container>
+                    <table className="text-center" style={{background: 'none', backgroundColor: '#2d2d2d', border:'none'}}>
+                        <thead style={{color: "white", backgroundColor:'#151515'}}>
+                        <tr>
+                            <th>Id</th>
+                            <th>Image</th>
+                            <th>Url</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody style={{color: "white"}}>
+                        {imageData &&
+                            imageData.map((img) => (
+
+                                <tr key={img.id}>
+                                    <td>{img.id}</td>
+                                    <td>
+                                        <img style={{boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}
+                                             src={img.imageUrl}
+                                             alt="image here" width="100px"/>
+                                    </td>
+                                    <td><p>{img.imageUrl}</p></td>
+                                    <td>
+                                        <Button type={"button"}></Button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                    </table>
+                </Container>
+                <Container>
                     <Card>
                         <Row>
                             <Col>
-                                <img style={{boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}
+                                <Image style={{boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}
                                      src="https://res.cloudinary.com/dyo1h8cbk/image/upload/v1683800293/sdcxrkoyhaarvdtzclf0.jpg"
                                      alt="image here" width="200px"/>
                             </Col>
@@ -34,7 +71,7 @@ export function Images() {
                                 }}>
                                     <p>https://res.cloudinary.com/dyo1h8cbk/image/upload/v1683800293/sdcxrkoyhaarvdtzclf0.jpg</p>
                                     <span></span>
-                                    <i onClick={copyToClipboard} className="bi bi-clipboard"></i>
+                                    <i className="bi bi-clipboard"></i>
                                 </div>
                             </Col>
                         </Row>
